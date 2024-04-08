94e17af5170307c9b0dab8a39e05681b0482862f
alec-nl <yoonsung@noncelab.com>
2024-04-08 18:31:11 +0900

    totalSupply call 추가, 코드 순서 정리, variable.js 삭제

--- test/task/interactWithContract.js
+++ test/task/interactWithContract.js
@@ -25,6 +25,7 @@ const callFunctions = [
   "balanceOf",
   "allowance",
   "totalPartitions",
+  "totalSupply",
   "totalSupplyByPartition",
   "partitionsOf",
   "balanceOfByPartition",
@@ -37,16 +38,22 @@ const callFunctions = [
 ];
 
 const sendFunctions = [
-  "approve",
-  "redeem",
-  "redeemFrom",
-  "redeemByPartition",
-  "operatorRedeemByPartition",
-  "approveByPartition",
   "authorizeOperator",
   "revokeOperator",
   "authorizeOperatorByPartition",
   "revokeOperatorByPartition",
+  "approve",
+  "approveByPartition",
+  "transfer",
+  "transferWithData",
+  "transferFrom",
+  "transferFromWithData",
+  "transferByPartition",
+  "operatorTransferByPartition",
+  "redeem",
+  "redeemFrom",
+  "redeemByPartition",
+  "operatorRedeemByPartition",
 ];
 
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.RPC_URL));
@@ -91,6 +98,7 @@ const functionCheck = () => {
               "isIssuable",
               "totalPartitions",
               "controllers",
+              "totalSupply",
             ].includes(input[1])
           ) {
             // 파라미터가 필요 없는 함수인 경우
@@ -149,32 +157,32 @@ const connectArgument = (input, parameters) => {
     case "granularity":
       granularity();
       break;
-    case "isControllable":
-      isControllable();
+    case "controllers":
+      controllers();
       break;
-    case "isIssuable":
-      isIssuable();
-      break;
-    case "balanceOf":
-      balanceOf(parameters);
-      break;
-    case "allowance":
-      allowance(parameters);
+    case "controllersByPartition":
+      controllersByPartition(parameters);
       break;
     case "totalPartitions":
       totalPartitions();
       break;
-    case "totalSupplyByPartition":
-      totalSupplyByPartition(parameters);
-      break;
     case "partitionsOf":
       partitionsOf(parameters);
       break;
-    case "balanceOfByPartition":
-      balanceOfByPartition(parameters);
+    case "totalSupply":
+      totalSupply();
       break;
-    case "allowanceByPartition":
-      allowanceByPartition(parameters);
+    case "totalSupplyByPartition":
+      totalSupplyByPartition(parameters);
+      break;
+    case "isIssuable":
+      isIssuable();
+      break;
+    case "isControllable":
+      isControllable();
+      break;
+    case "isMinter":
+      isMinter(parameters);
       break;
     case "isOperator":
       isOperator(parameters);
@@ -182,28 +190,20 @@ const connectArgument = (input, parameters) => {
     case "isOperatorForPartition":
       isOperatorForPartition(parameters);
       break;
-    case "controllers":
-      controllers();
+    case "balanceOf":
+      balanceOf(parameters);
       break;
-    case "controllersByPartition":
-      controllersByPartition(parameters);
+    case "balanceOfByPartition":
+      balanceOfByPartition(parameters);
       break;
-    case "isMinter":
-      isMinter(parameters);
+    case "allowance":
+      allowance(parameters);
       break;
+    case "allowanceByPartition":
+      allowanceByPartition(parameters);
+      break;
+
     // Send functions
-    case "approve":
-      break;
-    case "redeem":
-      break;
-    case "redeemFrom":
-      break;
-    case "redeemByPartition":
-      break;
-    case "operatorRedeemByPartition":
-      break;
-    case "approveByPartition":
-      break;
     case "authorizeOperator":
       break;
     case "revokeOperator":
@@ -211,6 +211,30 @@ const connectArgument = (input, parameters) => {
     case "authorizeOperatorByPartition":
       break;
     case "revokeOperatorByPartition":
+      break;
+    case "approve":
+      break;
+    case "approveByPartition":
+      break;
+    case "transfer":
+      break;
+    case "transferWithData":
+      break;
+    case "transferFrom":
+      break;
+    case "transferFromWithData":
+      break;
+    case "transferByPartition":
+      break;
+    case "operatorTransferByPartition":
+      break;
+    case "redeem":
+      break;
+    case "redeemByPartition":
+      break;
+    case "redeemFrom":
+      break;
+    case "operatorRedeemByPartition":
       break;
   }
 };
@@ -256,12 +280,11 @@ const granularity = async () => {
 };
 
 /**
- * @dev ERC-1400 컨트랙트의 isControllable 함수를 호출하여 operator에 의해 토큰 동작이 제어될 수 있는지
- *      여부를 조회합니다. 초기 설정 값은 true이며, 이를 변경하기 위해서는 renounceControl 함수를 호출해야 합니다.
- * @return {bool} _isControllable
+ * @dev ERC-1400 컨트랙트의 controllers 함수를 호출하여 전체 컨트롤러 리스트를 조회합니다.
+ * @return {address[]} _controllers
  */
-const isControllable = async () => {
-  const result = await contract.methods.isControllable().call();
+const controllers = async () => {
+  const result = await contract.methods.controllers().call();
 
   console.log(result);
 
@@ -269,54 +292,20 @@ const isControllable = async () => {
 };
 
 /**
- * @dev ERC-1400 컨트랙트의 isIssuable 함수를 호출하여 minter에 의해 토큰이 발행될 수 있는지
- *      여부를 조회합니다. 초기 설정 값은 true이며, 이를 변경하기 위해서는 renounceIssuance 함수를 호출해야 합니다.
- * @return {bool} _isIssuable
+ * @dev ERC-1400 컨트랙트의 controllersByPartition 함수를 호출하여 전체 파티션별 컨트롤러 리스트를 조회합니다.
+ * @param {string} partition - 파티션
+ * @return {address[]} _controllersByPartition[partition]
  */
-const isIssuable = async () => {
-  const result = await contract.methods.isIssuable().call();
-
-  console.log(result);
-
-  return result;
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 balanceOf 함수를 호출하여 조회 대상의 토큰 개수를 조회합니다.
- * @param {address} tokenHolder - 조회 대상의 주소
- * @return {uint256} _balances[tokenHolder]
- */
-const balanceOf = async (params) => {
-  if (params && params.length === 1 && web3.utils.isAddress(params[0])) {
-    const result = await contract.methods.balanceOf(params[0]).call();
-
-    console.log(Number(result));
-
-    return Number(result);
-  } else handleError(1, "call");
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 allowance 함수를 호출하여 사용 대상이 제어할 수 있는 조회 대상의 토큰 개수를 조회합니다.
- * @param {address} owner - 조회 대상의 주소
- * @param {address} spender - 조회 대상의 주소
- * @return {uint256} _allowed[owner][spender]
- */
-const allowance = async (params) => {
-  if (
-    params &&
-    params.length === 2 &&
-    web3.utils.isAddress(params[0]) &&
-    web3.utils.isAddress(params[1])
-  ) {
+const controllersByPartition = async (params) => {
+  if (params && params.length === 1 && params[0].length > 0) {
     const result = await contract.methods
-      .allowance(params[0], params[1])
+      .controllersByPartition(web3.utils.toHex(params[0]).padEnd(66, "0"))
       .call();
 
-    console.log(Number(result));
+    console.log(result);
 
-    return Number(result);
-  } else handleError(2, "call");
+    return result;
+  } else handleError(1, "call");
 };
 
 /**
@@ -336,23 +325,6 @@ const totalPartitions = async () => {
   console.log(tempResult);
 
   return tempResult;
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 totalSupplyByPartition 함수를 호출하여 파티션별 총 토큰 개수를 조회합니다.
- * @param {string} partition - 파티션
- * @return {uint256} _totalSupplyByPartition[partition]
- */
-const totalSupplyByPartition = async (params) => {
-  if (params && params.length === 1 && params[0].length > 0) {
-    const result = await contract.methods
-      .totalSupplyByPartition(web3.utils.toHex(params[0]).padEnd(66, "0"))
-      .call();
-
-    console.log(Number(result));
-
-    return Number(result);
-  } else handleError(3, "call");
 };
 
 /**
@@ -376,7 +348,149 @@ const partitionsOf = async (params) => {
     console.log(tempResult);
 
     return tempResult;
+  } else handleError(2, "call");
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 totalSupply 함수를 호출하여 토큰의 총 개수를 조회합니다.
+ * @return {uint256} _totalSupply
+ */
+const totalSupply = async () => {
+  const result = await contract.methods.totalSupply().call();
+
+  console.log(Number(result));
+
+  return Number(result);
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 totalSupplyByPartition 함수를 호출하여 파티션별 총 토큰 개수를 조회합니다.
+ * @param {string} partition - 파티션
+ * @return {uint256} _totalSupplyByPartition[partition]
+ */
+const totalSupplyByPartition = async (params) => {
+  if (params && params.length === 1 && params[0].length > 0) {
+    const result = await contract.methods
+      .totalSupplyByPartition(web3.utils.toHex(params[0]).padEnd(66, "0"))
+      .call();
+
+    console.log(Number(result));
+
+    return Number(result);
+  } else handleError(3, "call");
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 isIssuable 함수를 호출하여 minter에 의해 토큰이 발행될 수 있는지
+ *      여부를 조회합니다. 초기 설정 값은 true이며, 이를 변경하기 위해서는 renounceIssuance 함수를 호출해야 합니다.
+ * @return {bool} _isIssuable
+ */
+const isIssuable = async () => {
+  const result = await contract.methods.isIssuable().call();
+
+  console.log(result);
+
+  return result;
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 isControllable 함수를 호출하여 operator에 의해 토큰 동작이 제어될 수 있는지
+ *      여부를 조회합니다. 초기 설정 값은 true이며, 이를 변경하기 위해서는 renounceControl 함수를 호출해야 합니다.
+ * @return {bool} _isControllable
+ */
+const isControllable = async () => {
+  const result = await contract.methods.isControllable().call();
+
+  console.log(result);
+
+  return result;
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 isMinter 함수를 호출하여 조회 대상이 minter인지 조회합니다.
+ * @param {address} account - 조회 대상의 주소
+ * @return {bool} _minters.has(account)
+ */
+const isMinter = async (params) => {
+  if (params && params.length === 1 && web3.utils.isAddress(params[0])) {
+    const result = await contract.methods
+      .isMinter(web3.utils.toChecksumAddress(params[0]))
+      .call();
+
+    console.log(result);
+
+    return result;
+  }
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 isOperator 함수를 호출하여 제어 권한자가 조회 대상의 operator인지 조회합니다.
+ * @param {address} operator - 제어 권한자의 주소
+ * @param {address} tokenHolder - 조회 대상의 주소
+ * @return {bool} _isOperator(operator, tokenHolder)
+ */
+const isOperator = async (params) => {
+  if (
+    params &&
+    params.length === 2 &&
+    web3.utils.isAddress(params[0]) &&
+    web3.utils.isAddress(params[1])
+  ) {
+    const result = await contract.methods
+      .isOperator(
+        web3.utils.toChecksumAddress(params[0]),
+        web3.utils.toChecksumAddress(params[1])
+      )
+      .call();
+
+    console.log(result);
+
+    return result;
   } else handleError(4, "call");
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 isOperatorForPartition 함수를 호출하여 파티션별 제어 권한자가 조회 대상의 operator인지 조회합니다.
+ * @param {string} partition - 파티션
+ * @param {address} operator - 제어 권한자의 주소
+ * @param {address} tokenHolder - 조회 대상의 주소
+ * @return {bool} _isOperatorForPartition(partition, operator, tokenHolder)
+ */
+const isOperatorForPartition = async (params) => {
+  if (
+    params &&
+    params.length === 3 &&
+    params[0].length > 0 &&
+    web3.utils.isAddress(params[1]) &&
+    web3.utils.isAddress(params[2])
+  ) {
+    const result = await contract.methods
+      .isOperatorForPartition(
+        web3.utils.toHex(params[0]).padEnd(66, "0"),
+        web3.utils.toChecksumAddress(params[1]),
+        web3.utils.toChecksumAddress(params[2])
+      )
+      .call();
+
+    console.log(result);
+
+    return result;
+  } else handleError(5, "call");
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 balanceOf 함수를 호출하여 조회 대상의 토큰 개수를 조회합니다.
+ * @param {address} tokenHolder - 조회 대상의 주소
+ * @return {uint256} _balances[tokenHolder]
+ */
+const balanceOf = async (params) => {
+  if (params && params.length === 1 && web3.utils.isAddress(params[0])) {
+    const result = await contract.methods.balanceOf(params[0]).call();
+
+    console.log(Number(result));
+
+    return Number(result);
+  } else handleError(6, "call");
 };
 
 /**
@@ -402,7 +516,30 @@ const balanceOfByPartition = async (params) => {
     console.log(Number(result));
 
     return Number(result);
-  } else handleError(5, "call");
+  } else handleError(7, "call");
+};
+
+/**
+ * @dev ERC-1400 컨트랙트의 allowance 함수를 호출하여 사용 대상이 제어할 수 있는 조회 대상의 토큰 개수를 조회합니다.
+ * @param {address} owner - 조회 대상의 주소
+ * @param {address} spender - 조회 대상의 주소
+ * @return {uint256} _allowed[owner][spender]
+ */
+const allowance = async (params) => {
+  if (
+    params &&
+    params.length === 2 &&
+    web3.utils.isAddress(params[0]) &&
+    web3.utils.isAddress(params[1])
+  ) {
+    const result = await contract.methods
+      .allowance(params[0], params[1])
+      .call();
+
+    console.log(Number(result));
+
+    return Number(result);
+  } else handleError(8, "call");
 };
 
 /**
@@ -432,108 +569,7 @@ const allowanceByPartition = async (params) => {
     console.log(Number(result));
 
     return Number(result);
-  } else handleError(6, "call");
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 isOperator 함수를 호출하여 제어 권한자가 조회 대상의 operator인지 조회합니다.
- * @param {address} operator - 제어 권한자의 주소
- * @param {address} tokenHolder - 조회 대상의 주소
- * @return {bool} _isOperator(operator, tokenHolder)
- */
-const isOperator = async (params) => {
-  if (
-    params &&
-    params.length === 2 &&
-    web3.utils.isAddress(params[0]) &&
-    web3.utils.isAddress(params[1])
-  ) {
-    const result = await contract.methods
-      .isOperator(
-        web3.utils.toChecksumAddress(params[0]),
-        web3.utils.toChecksumAddress(params[1])
-      )
-      .call();
-
-    console.log(result);
-
-    return result;
-  } else handleError(7, "call");
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 isOperatorForPartition 함수를 호출하여 파티션별 제어 권한자가 조회 대상의 operator인지 조회합니다.
- * @param {string} partition - 파티션
- * @param {address} operator - 제어 권한자의 주소
- * @param {address} tokenHolder - 조회 대상의 주소
- * @return {bool} _isOperatorForPartition(partition, operator, tokenHolder)
- */
-const isOperatorForPartition = async (params) => {
-  if (
-    params &&
-    params.length === 3 &&
-    params[0].length > 0 &&
-    web3.utils.isAddress(params[1]) &&
-    web3.utils.isAddress(params[2])
-  ) {
-    const result = await contract.methods
-      .isOperatorForPartition(
-        web3.utils.toHex(params[0]).padEnd(66, "0"),
-        web3.utils.toChecksumAddress(params[1]),
-        web3.utils.toChecksumAddress(params[2])
-      )
-      .call();
-
-    console.log(result);
-
-    return result;
-  } else handleError(8, "call");
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 controllers 함수를 호출하여 전체 컨트롤러 리스트를 조회합니다.
- * @return {address[]} _controllers
- */
-const controllers = async () => {
-  const result = await contract.methods.controllers().call();
-
-  console.log(result);
-
-  return result;
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 controllersByPartition 함수를 호출하여 전체 파티션별 컨트롤러 리스트를 조회합니다.
- * @param {string} partition - 파티션
- * @return {address[]} _controllersByPartition[partition]
- */
-const controllersByPartition = async (params) => {
-  if (params && params.length === 1 && params[0].length > 0) {
-    const result = await contract.methods
-      .controllersByPartition(web3.utils.toHex(params[0]).padEnd(66, "0"))
-      .call();
-
-    console.log(result);
-
-    return result;
   } else handleError(9, "call");
-};
-
-/**
- * @dev ERC-1400 컨트랙트의 isMinter 함수를 호출하여 조회 대상이 minter인지 조회합니다.
- * @param {address} account - 조회 대상의 주소
- * @return {bool} _minters.has(account)
- */
-const isMinter = async (params) => {
-  if (params && params.length === 1 && web3.utils.isAddress(params[0])) {
-    const result = await contract.methods
-      .isMinter(web3.utils.toChecksumAddress(params[0]))
-      .call();
-
-    console.log(result);
-
-    return result;
-  }
 };
 
 // ####################################################
